9076eeca76d7ae6e060cfd6c052cff49d4292832
spencerj21 <spencerj21@chcstudent.com>
2020-07-24 23:26:16 -0400

    update test for controller bounding, charts, and gauges

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,20 @@ std::shared_ptr<kappa::InputLogger<double>> input;
 std::shared_ptr<kappa::PidController> controller;
 
 void opcontrol() {
+  auto chart = lv_chart_create(lv_scr_act(), NULL);
+  lv_obj_set_pos(chart, 0, 0);
+  lv_obj_set_size(chart, 480, 120);
+  lv_chart_set_type(chart, LV_CHART_TYPE_LINE);
+  lv_chart_set_range(chart, -12000, 12000);
+  lv_chart_set_point_count(chart, 60);
+  auto ser1 = lv_chart_add_series(chart, LV_COLOR_RED);
+  auto ser2 = lv_chart_add_series(chart, LV_COLOR_BLUE);
+
+  auto gauge = lv_gauge_create(lv_scr_act(), NULL);
+  lv_obj_set_size(gauge, 120, 120);
+  lv_obj_set_pos(gauge, 0, 120);
+  static lv_color_t ncolor[] = {LV_COLOR_RED};
+  lv_gauge_set_needle_count(gauge, 1, ncolor);
 
   chassis =
     std::make_shared<kappa::TupleOutputLogger<double,double>>(6, " Tuple Logger ", " | ", "\n",
@@ -26,16 +40,16 @@ void opcontrol() {
             std::make_shared<kappa::ArrayDistributor<double,2>>(
               kappa::ArrayDistributor<double,2>({
                 std::make_shared<kappa::VPidSubController>(
-                  kappa::VPidSubController::Gains{50,0,50,2000}, 20.0/3.0,
+                  kappa::VPidSubController::Gains{50,0,50,2000}, 20.0/3.0, -12000, 12000,
                   std::make_shared<kappa::OkapiInput>(std::make_shared<okapi::IntegratedEncoder>(19)),
-                  std::make_shared<kappa::OutputLogger<double>>(6, " Output Logger 1 ", "\n\n",
+                  std::make_shared<kappa::OutputChartLogger<double>>(chart, ser1,
                     std::make_shared<kappa::VoltageMotor>(std::make_shared<okapi::Motor>(19))
                   )
                 ),
                 std::make_shared<kappa::VPidSubController>(
-                  kappa::VPidSubController::Gains{50,0,50,2000}, 20.0/3.0,
+                  kappa::VPidSubController::Gains{50,0,50,2000}, 20.0/3.0, -12000, 12000,
                   std::make_shared<kappa::OkapiInput>(std::make_shared<okapi::IntegratedEncoder>(20)),
-                  std::make_shared<kappa::OutputLogger<double>>(6, " Output Logger 2 ", "\n\n",
+                  std::make_shared<kappa::OutputChartLogger<double>>(chart, ser2,
                     std::make_shared<kappa::VoltageMotor>(std::make_shared<okapi::Motor>(20))
                   )
                 ),
@@ -48,8 +62,10 @@ void opcontrol() {
 
   input =
     std::make_shared<kappa::InputLogger<double>>(6, " Input Logger ", "\n\n",
-      std::make_shared<kappa::OkapiInput>(
-        std::make_shared<okapi::ADIEncoder>(3,4)
+      std::make_shared<kappa::InputGaugeLogger<double>>(gauge, 0,
+        std::make_shared<kappa::OkapiInput>(
+          std::make_shared<okapi::ADIEncoder>(3,4)
+        )
       )
     );
 
