62bec09f39c918deaba52920f4e76f70c54fb64a
spencerj21 <spencerj21@chcstudent.com>
2020-10-10 17:58:54 -0400

    finish header documentation for inputs

--- include/kappa/input/modifier/arrayConsolidator.hpp
+++ include/kappa/input/modifier/arrayConsolidator.hpp
@@ -11,20 +11,35 @@ namespace kappa {
 template<typename T, std::size_t N>
 class ArrayConsolidator : public AbstractInput<std::array<T,N>> {
 public:
+
+  /**
+   * Packages the data from an array of inputs into a single array of data
+   *
+   * @param iinput inputs for data
+   */
   ArrayConsolidator(std::initializer_list<std::shared_ptr<AbstractInput<T>>> iinput) {
     if (iinput.size() != N) throw std::invalid_argument("Invalid number of args in kappa::ArrayConsolidator");
 
     std::copy(iinput.begin(), iinput.end(), input.begin());
   }
 
-
+  /**
+   * Gets data from its input, logs it, and returns the data
+   *
+   * @return input data
+   */
   virtual const std::array<T,N> &get() override {
     for(std::size_t i = 0; i < N; i++) {
       out[i] = input[i]->get();
     }
     return out;
   }
-
+  
+  /**
+   * Gets input source
+   *
+   * @return input
+   */
   std::array<std::shared_ptr<AbstractInput<T>>,N> getInput() const {
     return input;
   };
