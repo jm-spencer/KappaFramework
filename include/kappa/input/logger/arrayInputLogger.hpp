f085e98e0e92b25bbda6801ad4a65aaa17cdbb41
spencerj21 <spencerj21@chcstudent.com>
2020-10-10 17:41:16 -0400

    add header documentation to input loggers

--- include/kappa/input/logger/arrayInputLogger.hpp
+++ include/kappa/input/logger/arrayInputLogger.hpp
@@ -14,15 +14,41 @@ namespace kappa {
 template <typename T, std::size_t N>
 class ArrayInputLogger : public AbstractInput<std::array<T,N>> {
 public:
-  ArrayInputLogger(std::shared_ptr<AbstractInput<std::array<T,N>>> iinput):
-    ArrayInputLogger(" ", " ", "\n", std::cout, iinput) {}
 
+  /**
+   * Logs array data that passes through it. By default logs to std::cout,
+   * but can also log to filestreams
+   * Assumes operator<<(std::ostream,T) is defined
+   *
+   * @param iinput input for data
+   */
+  ArrayInputLogger(std::shared_ptr<AbstractInput<std::array<T,N>>> iinput):
+    ArrayInputLogger(", ", ", ", "\n", std::cout, iinput) {}
+
+  /**
+   * @param iprefix string that preceeds each line of data
+   * @param iseparator string that is printed between each element of data
+   * @param ipostfix string that follows each line of data (like a newline char)
+   * @param iinput input for data
+   */
   ArrayInputLogger(std::string iprefix, std::string iseparator, std::string ipostfix, std::shared_ptr<AbstractInput<std::array<T,N>>> iinput):
     ArrayInputLogger(iprefix, iseparator, ipostfix, std::cout, iinput) {}
 
+  /**
+   * @param iprefix string that preceeds each line of data
+   * @param iseparator string that is printed between each element of data
+   * @param ipostfix string that follows each line of data (like a newline char)
+   * @param iout ostream to print data to
+   * @param iinput input for data
+   */
   ArrayInputLogger(std::string iprefix, std::string iseparator, std::string ipostfix, std::ostream &iout, std::shared_ptr<AbstractInput<std::array<T,N>>> iinput):
     input(iinput), prefix(iprefix), separator(iseparator), postfix(ipostfix), out(iout) {}
 
+  /**
+   * Gets data from its input, logs it, and returns the data
+   *
+   * @return input data
+   */
   virtual const std::array<T,N> &get() override {
     const std::array<T,N> &values = input->get();
 
@@ -37,6 +63,11 @@ public:
     return values;
   }
 
+  /**
+   * Gets input source
+   *
+   * @return input
+   */
   std::shared_ptr<AbstractInput<std::array<T,N>>> getInput() const {
     return input;
   }
--- include/kappa/input/logger/tupleInputLogger.hpp
+++ include/kappa/input/logger/tupleInputLogger.hpp
@@ -12,18 +12,44 @@
 
 namespace kappa {
 
-template <typename... T>
+template <typename...T>
 class TupleInputLogger : public AbstractInput<std::tuple<T...>> {
 public:
+
+  /**
+   * Logs tuple data that passes through it. By default logs to std::cout,
+   * but can also log to filestreams
+   * Assumes operator<<(std::ostream,T) is defined
+   *
+   * @param iinput input for data
+   */
   TupleInputLogger(std::shared_ptr<AbstractInput<std::tuple<T...>>> iinput):
     TupleInputLogger(", ", ", ", "\n", std::cout, iinput) {}
 
+  /**
+   * @param iprefix string that preceeds each line of data
+   * @param iseparator string that is printed between each element of data
+   * @param ipostfix string that follows each line of data (like a newline char)
+   * @param iinput input for data
+   */
   TupleInputLogger(std::string iprefix, std::string iseparator, std::string ipostfix, std::shared_ptr<AbstractInput<std::tuple<T...>>> iinput):
     TupleInputLogger(prefix, iseparator, ipostfix, std::cout, iinput) {}
 
+  /**
+   * @param iprefix string that preceeds each line of data
+   * @param iseparator string that is printed between each element of data
+   * @param ipostfix string that follows each line of data (like a newline char)
+   * @param iout ostream to print data to
+   * @param iinput input for data
+   */
   TupleInputLogger(std::string iprefix, std::string iseparator, std::string ipostfix, std::ostream &iout, std::shared_ptr<AbstractInput<std::tuple<T...>>> iinput):
     input(iinput), prefix(iprefix), separator(iseparator), postfix(ipostfix), out(iout) {}
 
+  /**
+   * Gets data from its input, logs it, and returns the data
+   *
+   * @return input data
+   */
   virtual const std::tuple<T...> &get() override {
     const std::tuple<T...> &values = input->get();
 
@@ -36,6 +62,11 @@ public:
     return values;
   }
 
+  /**
+   * Gets input source
+   *
+   * @return input
+   */
   std::shared_ptr<AbstractInput<std::tuple<T...>>> getInput() const {
     return input;
   }
