e64a79abda891f7013bf50a17b65882e082339cc
spencerj21 <spencerj21@chcstudent.com>
2020-10-11 00:32:22 -0400

    add header documentation to outputs impls

--- include/kappa/output/impl/nullOutput.hpp
+++ include/kappa/output/impl/nullOutput.hpp
@@ -13,8 +13,16 @@ namespace kappa {
 template<typename T>
 class NullOutput : public AbstractOutput<T> {
 public:
+
+  /**
+   * Does nothing. If you want to test a controller without motors (or are running this on a computer)
+   * Use a logger piping to this. Loggers will assume NullOutput if no output is specified.
+   */
   NullOutput() = default;
 
+  /**
+   * Does nothing
+   */
   virtual void set(const T &itarget) override { }
 
 };
--- include/kappa/output/impl/twoAxisChassis.hpp
+++ include/kappa/output/impl/twoAxisChassis.hpp
@@ -16,12 +16,41 @@ namespace kappa {
 
 class TwoAxisChassis : public AbstractOutput<std::tuple<double,double>> {
 public:
+
+  /**
+   * Chassis object for a chassis with 2 degrees of freedom (nonholonomic)
+   * Converts an input signal of (forward, ccw)
+   * to an signal of (left, right)
+   * for an skid-steer like chassis
+   *
+   * @param ichassis array of motors (or motor groups) in format (left, right)
+   */
   TwoAxisChassis(std::shared_ptr<AbstractOutput<std::array<double,2>>> ichassis);
 
+  /**
+   * Chassis object for a chassis with 2 degrees of freedom (nonholonomic)
+   * Converts an input signal of (forward, ccw) in units of (in/s, rad/s)
+   * to an signal of (left, right) in units of rpm
+   * for an skid-steer like chassis
+   *
+   * @param iwheelDiameter diameter of wheel in inches
+   * @param ichassisWidth distance between parallel wheels in inches
+   * @param ichassis array of motors (or motor groups) in format (left, right)
+   */
   TwoAxisChassis(double iwheelDiameter, double ichassisWidth, std::shared_ptr<AbstractOutput<std::array<double,2>>> ichassis);
 
+  /**
+   * calculates target values for each motor and sets their respective targets
+   *
+   * @param itarget target values
+   */
   virtual void set(const std::tuple<double,double> &itarget) override;
 
+  /**
+   * Gets output
+   *
+   * @return output
+   */
   std::shared_ptr<AbstractOutput<std::array<double,2>>> getOutput() const;
 
 protected:
